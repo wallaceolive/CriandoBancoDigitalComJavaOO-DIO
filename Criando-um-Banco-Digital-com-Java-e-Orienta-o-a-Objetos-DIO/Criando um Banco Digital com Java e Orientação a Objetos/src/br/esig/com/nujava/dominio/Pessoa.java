eccf298e9630f4f6a8f1beb9918174fe3168b334
wallaceolive <wallaceolive@gmail.com>
2022-03-27 21:22:14 -0300

    banco digital em java

---
+++ Criando-um-Banco-Digital-com-Java-e-Orienta-o-a-Objetos-DIO/Criando um Banco Digital com Java e Orientação a Objetos/src/br/esig/com/nujava/dominio/Pessoa.java
@@ -0,0 +1,57 @@
+package br.esig.com.nujava.dominio;
+
+import java.util.Date;
+
+/** Esta entendidade representa uma Pessoa.
+ * 
+ * Uma pessoa possui CPF único. <p><br />
+ * Uma pessoa também faz um relacionamento com {@link Genero}. <p><br />
+ * 
+ * @author Wallace Oliveira
+ * @since 23/07/2022
+ * @version 2.0
+ */
+public class Pessoa {
+	private String nome;
+	
+	private Date dataNascimento;
+	
+	private String cpf;
+	
+	private Genero genero;
+	
+	public Pessoa() {}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Genero getGenero() {
+		return genero;
+	}
+
+	public void setGenero(Genero genero) {
+		this.genero = genero;
+	}
+	
+}
---
+++ Criando-um-Banco-Digital-com-Java-e-Orienta-o-a-Objetos-DIO/README.md
@@ -0,0 +1,1 @@
+# Criando-um-Banco-Digital-com-Java-e-Orienta-o-a-Objetos
